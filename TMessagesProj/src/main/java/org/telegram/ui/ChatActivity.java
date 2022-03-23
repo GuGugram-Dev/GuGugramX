a8fe9648dec6857fec4def9139047208b9949871
blxueya <blxueya@outlook.com>
2022-03-23 18:18:53 +0800

    fix:cherry-pick

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -21153,7 +21153,6 @@ ChatActivity extends BaseFragment implements NotificationCenter.NotificationCent
                             boolean ignore = false;
                             int count = 0;
                             if (!GuGuConfig.INSTANCE.getAlwaySaveChatOffset().Bool()){
-                                int count = 0;
                                 for (int a = position - 1; a >= chatAdapter.messagesStartRow; a--) {
                                     int num = a - chatAdapter.messagesStartRow;
                                     if (num < 0 || num >= messages.size()) {
