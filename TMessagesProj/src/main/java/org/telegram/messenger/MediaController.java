103ad220b9be87eea6368a1e2183d26eb464761d
blxueya <blxueya@outlook.com>
2022-03-27 13:48:00 +0800

    feat: add noise suppress and voice enhance from Nagram

--- TMessagesProj/src/main/java/org/telegram/messenger/MediaController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MediaController.java
@@ -60,6 +60,7 @@ import android.widget.FrameLayout;
 
 import androidx.annotation.RequiresApi;
 
+import com.blxueya.GuGugramX.helper.AudioEnhance;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
@@ -3550,6 +3551,7 @@ public class MediaController implements AudioManager.OnAudioFocusChangeListener,
                 recordReplyingMsg = replyToMsg;
                 recordReplyingTopMsg = replyToTopMsg;
                 fileBuffer.rewind();
+                AudioEnhance.INSTANCE.initVoiceEnhance(audioRecorder);
 
                 audioRecorder.startRecording();
             } catch (Exception e) {
@@ -3559,6 +3561,7 @@ public class MediaController implements AudioManager.OnAudioFocusChangeListener,
                 recordingAudioFile.delete();
                 recordingAudioFile = null;
                 try {
+                    AudioEnhance.INSTANCE.releaseVoiceEnhance();
                     audioRecorder.release();
                     audioRecorder = null;
                 } catch (Exception e2) {
@@ -3652,6 +3655,7 @@ public class MediaController implements AudioManager.OnAudioFocusChangeListener,
             requestAudioFocus(false);
         }
         try {
+            AudioEnhance.INSTANCE.releaseVoiceEnhance();
             if (audioRecorder != null) {
                 audioRecorder.release();
                 audioRecorder = null;
